90eadcb63cf29c964f45d33985e8fb74a96f2915
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-10 13:48:23 -0500

    updating ./Learn-Shell

--- 02-shell.sh
+++ 02-shell.sh
@@ -11,7 +11,7 @@ echo -e "Hello\nWorld"
 echo -e "one\t\ttwo"
 
 # Color printing
-echo -e "\e[31mHello\e[0m"
+echo -e '\e[31mHello\e[0m'
 echo -e "\e[32mHello\e[0m"
 echo -e "\e[33mHello\e[0m"
 echo -e "\e[34mHello\e[0m"
