35bfa8a3eda8db566ae36706da69e964c03efa81
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-09 22:59:21 -0500

    updating ./Learn-Shell

---
+++ 03-shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+a=10
+echo Value of a = $a
+
+date=2022-08-05
+echo Today date is $date
+
+date1=$(date +%F)
+echo Todaye date is $date1
+
+b=$((2+3-5*6))
+echo Value of b = $b
+
+## This is env vari
+echo Value of c = $c
